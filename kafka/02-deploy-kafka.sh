6782026152ccf772c6f4d45f7408ca649a9a1550
Paolo Patierno <ppatierno@live.com>
2020-02-20 09:49:41 +0100

    Added sleeps waiting CO creating resources before checking status (#9)

--- kafka/02-deploy-kafka.sh
+++ kafka/02-deploy-kafka.sh
@@ -10,7 +10,7 @@ sed -i "s/my-kafka-version/$VERSION/" $DIR/cluster/$CLUSTER-kafka-persistent-wit
 
 oc apply -f $DIR/cluster/$CLUSTER-kafka-persistent-with-metrics.yaml -n $NAMESPACE
 
-# delay for allowing cluster operator to create the first Zookeeper statefulset
+# delay for allowing cluster operator to create the ZooKeeper statefulset
 sleep 5
 
 zkReplicas=$(oc get kafka $CLUSTER -o jsonpath="{.spec.zookeeper.replicas}" -n $NAMESPACE)
@@ -23,6 +23,9 @@ do
 done
 echo "...Zookeeper cluster ready"
 
+# delay for allowing cluster operator to create the Kafka statefulset
+sleep 5
+
 kReplicas=$(oc get kafka $CLUSTER -o jsonpath="{.spec.kafka.replicas}" -n $NAMESPACE)
 echo "Waiting for Kafka cluster to be ready..."
 readyReplicas="0"
@@ -33,11 +36,15 @@ do
 done
 echo "...Kafka cluster ready"
 
+# delay for allowing cluster operator to create the Entity Operator deployment
+sleep 5
+
 echo "Waiting for entity operator to be ready..."
 oc rollout status deployment/$CLUSTER-entity-operator -w -n $NAMESPACE
 echo "...entity operator ready"
 
-sleep 2
+# delay for allowing cluster operator to create the Kafka Exporter deployment
+sleep 5
 
 echo "Waiting for kafka exporter to be ready..."
 oc rollout status deployment/$CLUSTER-kafka-exporter -w -n $NAMESPACE
