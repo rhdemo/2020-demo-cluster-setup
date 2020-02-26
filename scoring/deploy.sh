f9daffceaa111ccf8671d2f45f49df1818381be8
Chris Chase <cchase@redhat.com>
2020-02-26 11:44:44 -0500

    Deploy new leaderboard and dashboard. Deploy kafka messaging scoring service.

--- scoring/deploy.sh
+++ scoring/deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -x
 
-printf "\n\n######## frontend/deploy ########\n"
+printf "\n\n######## scoring/deploy ########\n"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJECT=${PROJECT:-scoring}
+PROJECT=${PROJECT:-frontend}
 QUAY_ORG=${QUAY_ORG:-redhatdemo}
 
 oc project ${PROJECT} 2> /dev/null || oc new-project ${PROJECT}
 
 echo "Deploying Scoring Service"
 
-SCORING_SERVER_PARAMS="IMAGE_REPOSITORY=quay.io/${QUAY_ORG}/2020-scoring-server:latest -p REPLICAS=1"
+SCORING_SERVER_PARAMS="IMAGE_REPOSITORY=quay.io/${QUAY_ORG}/2020-scoring-server:latest -p REPLICAS=2"
 
 oc process -f "${DIR}/scoring-server.yml" ${SCORING_SERVER_PARAMS} | oc create -f -
 
