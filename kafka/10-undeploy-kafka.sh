47cff51d095c7b9b4bcaab1b93c386ba9d37acb7
Paolo Patierno <ppatierno@live.com>
2020-03-03 17:19:45 +0100

    Added undeploying Kafka Mirror Maker 2 (#25)

--- kafka/10-undeploy-kafka.sh
+++ kafka/10-undeploy-kafka.sh
@@ -4,6 +4,14 @@ NAMESPACE=${KAFKA_NAMESPACE:-kafka-demo}
 CLUSTER=${KAFKA_CLUSTER:-demo2020}
 
 # delete Kafka topics
+echo "Deleting KafkaTopic..."
 oc delete kafkatopic my-topic -n $NAMESPACE
+echo "...deleted"
+# delete Kafka Mirror Maker 2
+echo "Deleting Kafka Mirror Maker 2..."
+oc delete kafkamirrormaker2 $CLUSTER-mirror-maker-2 -n $NAMESPACE
+echo "...deleted"
 # delete Kafka cluster
+echo "Deleting Kafka cluster..."
 oc delete kafka $CLUSTER -n $NAMESPACE
+echo "...deleted"
