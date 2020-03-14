e06234a821b2f5ab7b7feee124b73417ff429d91
Paolo Patierno <ppatierno@live.com>
2020-03-14 15:17:50 +0100

    Used option flags for producer and consumer tests (#39)

--- kafka/test/consumer.sh
+++ kafka/test/consumer.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
 # usage:
-# consumer.sh <consumer-name> <bootstrap-server> <consumer-group> <topic>
+# consumer.sh -n <consumer-name> -r <replicas> -b <bootstrap-server> -g <consumer-group> -t <topic>
 # example:
-# consumer.sh consumer my-cluster-kafka-bootstrap:9092 my-group demo2.my-topic
+# consumer.sh -n consumer -r 1 -b demo2020-kafka-bootstrap:9092 -g my-group -t my-topic
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 echo "DIR="$DIR
 
-CONSUMER_NAME=$1
-BOOTSTRAP_SERVERS=$2
-GROUP_ID=$3
-TOPIC=$4
+# default values
+CONSUMER_NAME=my-consumer
+REPLICAS=1
+BOOTSTRAP_SERVERS=demo2020-kafka-bootstrap:9092
+GROUP_ID=my-group
+TOPIC=my-topic
 
-cat  $DIR/consumer.yaml \
-| sed "s/consumer_name/$CONSUMER_NAME/" \
-| sed "s/bootstrap_servers/$BOOTSTRAP_SERVERS/" \
-| sed "s/groupid/$GROUP_ID/" \
-| sed "s/topic/$TOPIC/" \
-| oc apply -f -
+while getopts n:r:b:g:t: option
+do
+    case ${option} in
+        n) CONSUMER_NAME=${OPTARG};;
+        r) REPLICAS=${OPTARG};;
+        b) BOOTSTRAP_SERVERS=${OPTARG};;
+        g) GROUP_ID=${OPTARG};;
+        t) TOPIC=${OPTARG};;
+    esac
+done
+
+echo "CONSUMER_NAME=$CONSUMER_NAME"
+echo "REPLICAS=$REPLICAS"
+echo "BOOTSTRAP_SERVERS=$BOOTSTRAP_SERVERS"
+echo "GROUP_ID=$GROUP_ID"
+echo "TOPIC=$TOPIC"
+
+cp $DIR/consumer.yaml $DIR/consumer-deployment.yaml
+
+yq w -i $DIR/consumer-deployment.yaml metadata.name $CONSUMER_NAME
+yq w -i $DIR/consumer-deployment.yaml metadata.labels.app $CONSUMER_NAME
+yq w -i $DIR/consumer-deployment.yaml spec.selector.matchLabels.app $CONSUMER_NAME
+yq w -i $DIR/consumer-deployment.yaml spec.template.metadata.labels.app $CONSUMER_NAME
+yq w -i $DIR/consumer-deployment.yaml spec.replicas $REPLICAS
+
+yq w -i $DIR/consumer-deployment.yaml spec.template.spec.containers[0].name $CONSUMER_NAME
+yq w -i $DIR/consumer-deployment.yaml spec.template.spec.containers[0].env.name==BOOTSTRAP_SERVERS.value $BOOTSTRAP_SERVERS
+yq w -i $DIR/consumer-deployment.yaml spec.template.spec.containers[0].env.name==TOPIC.value $TOPIC
+yq w -i $DIR/consumer-deployment.yaml spec.template.spec.containers[0].env.name==GROUP_ID.value $GROUP_ID
+
+#oc apply -f $DIR/consumer-deployment.yaml
+
+#rm $DIR/consumer-deployment.yaml
