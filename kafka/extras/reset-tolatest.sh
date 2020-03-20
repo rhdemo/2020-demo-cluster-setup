8639018b4d250a1f72ef8473e2b0a76419617815
Paolo Patierno <ppatierno@live.com>
2020-03-20 18:02:18 +0100

    Added reset for all groups on a topic (#55)

--- kafka/extras/reset-tolatest.sh
+++ kafka/extras/reset-tolatest.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -x
 
-# reset the offset for a consumer group on a topic to the latest one
+# reset the offset for a consumer group or all groups on a topic to the latest one
 #
 # usage:
-# reset-tolatest.sh -g <consumer-group> -t <topic>
+# reset-tolatest.sh [-g <consumer-group>|-a] -t <topic>
+# example: reset-tolatest.sh -g my-group -t my-topic
+# example: reset-tolatest.sh -a -t my-topic
 
 NAMESPACE=${KAFKA_NAMESPACE:-kafka-demo}
 CLUSTER=${KAFKA_CLUSTER:-demo2020}
@@ -11,28 +14,39 @@ CLUSTER=${KAFKA_CLUSTER:-demo2020}
 # default values
 CONSUMER_GROUP=my-group
 TOPIC=my-topic
+ALL_GROUPS=false
 
-while getopts g:t: option
+while getopts g:t:a option
 do
     case ${option} in
         g) CONSUMER_GROUP=${OPTARG};;
         t) TOPIC=${OPTARG};;
+        a) ALL_GROUPS=true
     esac
 done
 
 echo "CONSUMER_GROUP=$CONSUMER_GROUP"
 echo "TOPIC=$TOPIC"
+echo "ALL_GROUPS=$ALL_GROUPS"
 
-echo "Current status for consumer group [$CONSUMER_GROUP] ..."
+if [ "$ALL_GROUPS" == "false" ]; then
+    GROUP="--group $CONSUMER_GROUP"
+    GROUP_MESSAGE="consumer group [$CONSUMER_GROUP]"
+else
+    GROUP="--all-groups"
+    GROUP_MESSAGE="all consumer groups"
+fi
+
+echo "Current status for $GROUP_MESSAGE ..."
 oc exec $CLUSTER-kafka-0 -c kafka -n $NAMESPACE -- bin/kafka-consumer-groups.sh \
 --bootstrap-server $CLUSTER-kafka-bootstrap.$NAMESPACE.svc.cluster.local:9092 \
---group $CONSUMER_GROUP \
+$GROUP \
 --describe
 
-echo "Reset offsets to latest for consumer group [$CONSUMER_GROUP] on topic [$TOPIC] ... dry-run ..."
+echo "Reset offsets to latest for $GROUP_MESSAGE on topic [$TOPIC] ... dry-run ..."
 oc exec $CLUSTER-kafka-0 -c kafka -n $NAMESPACE -- bin/kafka-consumer-groups.sh \
 --bootstrap-server $CLUSTER-kafka-bootstrap.$NAMESPACE.svc.cluster.local:9092 \
---group $CONSUMER_GROUP \
+$GROUP \
 --topic $TOPIC \
 --reset-offsets --to-latest \
 --dry-run
@@ -40,7 +54,7 @@ oc exec $CLUSTER-kafka-0 -c kafka -n $NAMESPACE -- bin/kafka-consumer-groups.sh 
 read -e -p "Do you want to execute? [y|n] "
 
 if [ $REPLY == "y" ]; then
-    echo "Reset offsets to latest for consumer group [$CONSUMER_GROUP] on topic [$TOPIC] ... execute ..."
+    echo "Reset offsets to latest for $GROUP_MESSAGE on topic [$TOPIC] ... execute ..."
     oc exec $CLUSTER-kafka-0 -c kafka -n $NAMESPACE -- bin/kafka-consumer-groups.sh \
     --bootstrap-server $CLUSTER-kafka-bootstrap.$NAMESPACE.svc.cluster.local:9092 \
     --group $CONSUMER_GROUP \
@@ -48,7 +62,7 @@ if [ $REPLY == "y" ]; then
     --reset-offsets --to-latest \
     --execute
 
-    echo "New status for consumer group [$CONSUMER_GROUP] ..."
+    echo "New status for $GROUP_MESSAGE ..."
     oc exec $CLUSTER-kafka-0 -c kafka -n $NAMESPACE -- bin/kafka-consumer-groups.sh \
     --bootstrap-server $CLUSTER-kafka-bootstrap.$NAMESPACE.svc.cluster.local:9092 \
     --group $CONSUMER_GROUP \
