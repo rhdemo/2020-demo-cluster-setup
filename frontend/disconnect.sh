0bec1e30c77834b129ec1ebb78a8c488405afa01
Ryan Cook <rcook@redhat.com>
2020-03-27 15:32:19 -0400

    add and removal attempt within acm

--- frontend/disconnect.sh
+++ frontend/disconnect.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+read -p "What's the username? " username
+read -p "What's the password? " password
+
+# logout of whererver
+oc logout
+
+# login to acm to patch
+oc login -u ${username} -p ${password} --server=https://api.summit-aws-acm.openshift.redhatkeynote.com:6443
+oc patch cluster -n london london --type=json -p='[{"op": "remove", "path": "/metadata/labels/app"}]'
+oc logout
+
+
+# login to lnd to remove the items
+oc login -u ${username} -p ${password} api.summit-aws-lnd1.openshift.redhatkeynote.com:6443
+
 printf "\n\n######## disconnect from ha proxy ########\n"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT=${PROJECT:-frontend}
--- frontend/reconnect.sh
+++ frontend/reconnect.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+read -p "What's the username? " username
+read -p "What's the password? " password
+
+# logout of whererver
+oc logout
+
+# login to acm to patch
+oc login -u ${username} -p ${password} --server=https://api.summit-aws-acm.openshift.redhatkeynote.com:6443
+kubectl patch cluster -n london london --type=json -p='[{"op": "add", "path": "/metadata/labels", "value": {"app": "gpt","vendor": "OpenShift","cloud": "Amazon","purpose": "production"} }]'
+oc logout
+
+
+# login to lnd to remove the items
+oc login -u ${username} -p ${password} api.summit-aws-lnd1.openshift.redhatkeynote.com:6443
 
 printf "\n\n######## recreating routes ########\n"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
