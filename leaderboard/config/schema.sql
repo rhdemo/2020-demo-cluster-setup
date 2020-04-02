3f7078c167f0e549d40a4b91a3848d249d6bc4f1
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-04-03 00:34:37 +0530

    remove pipelines and update pgsql

--- leaderboard/config/schema.sql
+++ leaderboard/config/schema.sql
@@ -1,45 +1,47 @@
 DROP TABLE IF EXISTS "public"."games" CASCADE ;
 CREATE TABLE "public"."games" (
-    "game_id" character varying(255) NOT NULL,
-    "game_config" json,
-    "game_date" timestamptz,
-    "game_state" character varying(10) NOT NULL
+    "id" SERIAL,
+    "game_id" character varying(75) NOT NULL,
+    "game_config" jsonb,
+    "game_date" timestamptz NOT NULL default now(),
+    "game_state" smallint NOT NULL
 ) WITH (oids = false);
 
 
 DROP TABLE IF EXISTS "public"."players";
 CREATE TABLE "public"."players" (
-    "player_id" character varying(255) NOT NULL,
-    "player_name" character varying(255) NOT NULL,
-    "guess_right" integer NOT NULL,
-    "guess_wrong" integer NOT NULL,
-    "guess_score" integer NOT NULL,
-    "creation_server" character varying(255) NOT NULL,
-    "game_server" character varying(255) NOT NULL,
-    "scoring_server" character varying(255) NOT NULL,
-    "player_avatar" json NOT NULL,
-    "game_id" character varying(255) NOT NULL
+    "id" SERIAL,
+    "player_id" character varying(75) NOT NULL,
+    "player_name" character varying(75) NOT NULL,
+    "guess_right" smallint NOT NULL,
+    "guess_wrong" smallint NOT NULL,
+    "guess_score" smallint NOT NULL,
+    "creation_server" character varying(50) NOT NULL,
+    "game_server" character varying(50) NOT NULL,
+    "scoring_server" character varying(50) NOT NULL,
+    "player_avatar" jsonb NOT NULL,
+    "game_id" character varying(75) NOT NULL
 ) WITH (oids = false);
-
 
 -- Create Constraints
 
-ALTER TABLE "public"."games" ADD CONSTRAINT "games_pkey"  PRIMARY KEY ("game_id");
+ALTER TABLE "public"."games" ADD CONSTRAINT "games_pkey"  PRIMARY KEY (id);
 
-ALTER TABLE "public"."players" ADD CONSTRAINT "player_pkey"  PRIMARY KEY ("player_id");
+ALTER TABLE "public"."games" ADD CONSTRAINT "game_id_unique" UNIQUE (game_id);
+
+ALTER TABLE "public"."players" ADD CONSTRAINT "player_pkey"  PRIMARY KEY (id);
+
+ALTER TABLE "public"."players" ADD CONSTRAINT "player_id_unqiue"  UNIQUE (player_id);
 
 ALTER TABLE "public"."players" ADD CONSTRAINT "players_game_id_fkey" FOREIGN KEY (game_id) REFERENCES games(game_id) NOT DEFERRABLE;
 
 -- Create Indexes
 
-CREATE INDEX "idx_game_state" ON "public"."games" USING btree ("game_state");
+DROP INDEX IF EXISTS "idx_games";
+CREATE INDEX "idx_games" ON "public"."games" (game_date DESC);
 
-CREATE INDEX "idx_creation_server" ON "public"."players" USING btree ("creation_server");
+DROP INDEX IF EXISTS "idx_game_id";
+CREATE INDEX "idx_game_id" ON "public"."players" USING HASH (game_id);
 
-CREATE INDEX "idx_game_id" ON "public"."players" USING btree ("game_id");
-
-CREATE INDEX "idx_game_server" ON "public"."players" USING btree ("game_server");
-
-CREATE INDEX "idx_player_id" ON "public"."players" USING btree ("player_id");
-
-CREATE INDEX "idx_scoring_server" ON "public"."players" USING btree ("scoring_server");
+DROP INDEX IF EXISTS  "idx_players";
+CREATE INDEX "idx_players" ON "public"."players" (guess_score DESC,guess_right DESC, guess_wrong ASC);
