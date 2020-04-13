1a2b9700a60494563f57325fd73eaa9a01789394
Subin Modeel <sub-mod@redhat.com>
2020-04-12 20:22:22 -0400

    add IMAGE_REPOSITORY param

--- digit-recognition/deploy.sh
+++ digit-recognition/deploy.sh
@@ -13,7 +13,7 @@ oc project ${PROJECT} 2> /dev/null || oc new-project ${PROJECT}
 echo "Deploying digit_recognition Application"
 ML_PARAMS="IMAGE_REPOSITORY=quay.io/${QUAY_ORG}/2020-digit-recognition:latest \
     -p REPLICAS=12 \
-    -p TF_CPP_MIN_VLOG_LEVEL=0 \
+    -p IMAGE_REPOSITORY=quay.io/redhatdemo/2020-digit-recognition:latest \
     -p APPLICATION_NAME=tf-cnn"
 oc process -f "${DIR}/deployment.yml" ${ML_PARAMS} \
   | oc create -f -
